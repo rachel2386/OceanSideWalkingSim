71ff7fa038dff701b84bcfa20fabd98cfe817df6
marygeorgescu <mg3200@nyu.edu>
2018-10-29 22:00:11 -0400

    this is the audio script!

---
+++ ocean/Assets/Scripts/playonce.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playonce : MonoBehaviour
+{
+	private AudioClip myAudio;
+
+	private AudioSource myAS;
+
+	private bool hasPlayed = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		myAS = GetComponent<AudioSource>();
+		myAudio = myAS.clip;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log("hasPlayed" + hasPlayed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{ if (other.gameObject.tag.Equals("Player"))
+			if (!hasPlayed)
+			{
+				myAS.PlayOneShot(myAudio);
+				Debug.Log(other.gameObject.name);
+				hasPlayed = true;
+				
+			}
+	}
+}
