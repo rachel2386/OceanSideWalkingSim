ab30ac2a2720865f3c35108d7ccbc04fed712817
rachelqxl <rl2563@nyu.edu>
2018-11-05 14:02:07 -0500

    text UI added to show num of trash needed to open gate

--- ocean/Assets/Scripts/OpenGate.cs
+++ ocean/Assets/Scripts/OpenGate.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenGate : MonoBehaviour
 {
 	Animator myAnim;
 	private GameObject[] itemsWTag;
+	private Text CollectNum;
 
 	// Use this for initialization
 	void Start ()
 	{
 		myAnim = GetComponent<Animator>();
-		
-		
+		CollectNum = GameObject.Find("Text").GetComponent<Text>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		
 		itemsWTag = GameObject.FindGameObjectsWithTag("items");
 		Debug.Log("itemsNum = " + itemsWTag.Length );
+		if (itemsWTag.Length > 0)
+		{
+			CollectNum.text = "collectible remaining: " + itemsWTag.Length;
+		}
+
 		if (itemsWTag.Length <= 0)
 		{
-			
+			CollectNum.text = "collectible remaining: 0" ;
+			CollectNum.color = Color.yellow;
 			myAnim.SetBool("OpenGate",true);
 		}
 	}
