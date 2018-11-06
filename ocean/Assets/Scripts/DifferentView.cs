b6d23dc61a116e81b2d7ea9984f108ebfed0c23e
maryageorgescu <mg3200@nyu.edu>
2018-11-05 22:32:17 -0500

    City lights trigger.

---
+++ ocean/Assets/Scripts/DifferentView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DifferentView : MonoBehaviour
+{
+
+    public Animator CityLights;
+    
+
+    void Start()
+    {
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            CityLights.SetBool("TurnOnCity", true);
+        }
+     
+    }
+
+
+}
