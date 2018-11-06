46827fde45d047a14498ce8d831c1f5b1d73b35d
rachelqxl <rl2563@nyu.edu>
2018-11-06 00:24:46 -0500

    change camera to physical 70mm imax
    added gate open sound and trashCollect sound on trigger

---
+++ ocean/Assets/Scripts/disposeTrash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Purchasing;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class disposeTrash : MonoBehaviour
+{
+	private Animator trashAnim;
+	// Use this for initialization
+	void Start ()
+	{
+		trashAnim = GameObject.Find("TrashCan").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag.Equals("Player"))
+		{
+			//RaycastHit lookAtTube = new RaycastHit();
+			//if (Physics.Raycast(other.gameObject.transform.position, other.gameObject.transform.forward, out lookAtTube,
+			//	10f))
+			//{
+				if (Input.GetMouseButtonUp(0))
+				{
+					trashAnim.SetBool("trashDisposed", true);
+
+					if (GetComponent<AudioSource>().isPlaying)
+						GetComponent<AudioSource>().Play();
+
+				}
+			//}
+		}
+	}
+}
